93565c130f5b82c956755316f79a5fd57f763e67
ucs_zhangjiaheng <1196263517@qq.com>
2017-04-13 10:59:53 +0800

    add feature-1 on feature-1

--- app/src/main/java/com/ucsmy/pos/activity/MainActivity.java
+++ app/src/main/java/com/ucsmy/pos/activity/MainActivity.java
@@ -35,8 +35,10 @@ public class MainActivity extends AppCompatActivity {
         mTextView = (TextView) findViewById(R.id.cpu);
         CpuInfo cpuInfo = new CpuInfo();
         cpuInfo.calCpuRatioInfo();
-        mTextView.setText(cpuInfo.getProcessCpuRatio()+"/"+cpuInfo.getProcessCpuRate());
+//        mTextView.setText(cpuInfo.getProcessCpuRatio()+"/"+cpuInfo.getProcessCpuRate());
     }
+
+
 
     public void openCamera(View v){
         FosBusinessAgent agent = new FosBusinessAgent();
